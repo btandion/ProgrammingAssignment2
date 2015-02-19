2e1fbc1eb4ee71d55f7bf54454664d3de6edf578
btandion <btandion@users.noreply.github.com>
2015-02-19 11:37:23 -0800

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,19 @@
+
+# Create a template/blueprint/class for a matrix with four predefined operations(methods).
+# Invoking this function given a invertile square matrix will instantiate a "special matrix"(object). 
+# This object is encapsulated with functions which support inversion of the matrix and its associated storage.
+# 
+# The four pre-defined functions are:
+# set          - creates new 'special matrix' object and initializes its inverse
+# get          - echoes back the matrix 
+# setinverse   - takes a given inverse matrix as part of this object
+# getinverse   - echoes back the inverse matrix
+#
 makeCacheMatrix <- function(mx = matrix()) {
+  # initialize the inverse, imx
   imx <- NULL
+  
+  # set object to a new one(new input matrix) without invoking makeCacheMatrix
   set <- function(yx) {
     mx <<- yx
     imx <<- NULL
